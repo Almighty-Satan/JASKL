5275350cd2bef9128f5d0b39c8a449a6ac6376d5
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-03-27 17:35:02 +0200

    Improve build script

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -1,23 +1,8 @@
 plugins {
-    id("java")
-}
-
-group = "com.github.almightysatan"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    jaskl
 }
 
 dependencies {
     implementation(project(":core"))
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
-    compileOnly("org.jetbrains:annotations:24.0.1")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
