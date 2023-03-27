7c2a4c0ab140d41e81a75ef46fba0f3e470021a2
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-03-27 18:27:58 +0200

    Maven publishing

--- buildSrc/src/main/kotlin/jaskl.gradle.kts
+++ buildSrc/src/main/kotlin/jaskl.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("checkstyle")
+    id("maven-publish")
 }
 
 checkstyle {
@@ -8,8 +9,8 @@ checkstyle {
     toolVersion = "9.3"
 }
 
-group = "com.github.almightysatan"
-version = "1.0-SNAPSHOT"
+group = "com.github.almighty-satan"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -24,4 +25,27 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("JASKL")
+                description.set("Just Another Simple Konfig Library")
+                url.set("https://github.com/Almighty-Satan/JASKL")
+            }
+            artifactId = "jaskl-${project.name}"
+        }
+        repositories {
+            maven {
+                setUrl("https://repo.varoplugin.de/repository/maven-releases/")
+                credentials {
+                    username = project.findProperty("repouser") as? String
+                    password = project.findProperty("repopassword") as? String
+                }
+            }
+        }
+    }
 }
