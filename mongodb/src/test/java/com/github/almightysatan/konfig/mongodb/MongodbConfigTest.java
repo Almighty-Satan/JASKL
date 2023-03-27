c8f086e8bda37acfda8bf20d23f26f27a16ee7bb
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-03-27 16:49:40 +0200

    Mongodb implementation

---
+++ mongodb/src/test/java/com/github/almightysatan/konfig/mongodb/MongodbConfigTest.java
@@ -0,0 +1,43 @@
+package com.github.almightysatan.konfig.mongodb;
+
+import com.github.almightysatan.konfig.Config;
+import com.github.almightysatan.konfig.ConfigEntry;
+import com.github.almightysatan.konfig.entries.IntegerConfigEntry;
+import com.github.almightysatan.konfig.entries.StringConfigEntry;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MongodbConfigTest {
+
+    @Test
+    @Disabled
+    public void testWrite() throws IOException {
+        String mongoAddress = System.getenv("MONGO_ADDRESS");
+
+        Config config0 = MongodbConfig.of(mongoAddress, "configTest", "abc");
+        ConfigEntry<String> stringConfigEntry0 = StringConfigEntry.of(config0, "exampleString", null, "default");
+        ConfigEntry<Integer> intConfigEntry0 = IntegerConfigEntry.of(config0, "exampleInt", null, 69);
+
+        config0.load();
+
+        stringConfigEntry0.setValue("Example");
+        intConfigEntry0.setValue(420);
+
+        config0.write();
+        config0.close();
+
+        Config config1 = MongodbConfig.of(mongoAddress, "configTest", "abc");
+        ConfigEntry<String> stringConfigEntry1 = StringConfigEntry.of(config1, "exampleString", null, "default");
+        ConfigEntry<Integer> intConfigEntry1 = IntegerConfigEntry.of(config1, "exampleInt", null, 69);
+
+        config1.load();
+        config1.strip();
+
+        assertEquals("Example", stringConfigEntry1.getValue());
+        assertEquals(420, intConfigEntry1.getValue());
+    }
+}
