0ec011dfb986ff8caf6faeed18a018648aec339c
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-05-31 09:37:26 +0200

    Disable publishing of test fixtures

--- buildSrc/src/main/kotlin/jaskl.gradle.kts
+++ buildSrc/src/main/kotlin/jaskl.gradle.kts
@@ -39,6 +39,10 @@ tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+
 publishing {
     publications {
         create<MavenPublication>("release") {
