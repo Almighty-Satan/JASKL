abd0d70f31efa79fe4957d7ac5fa3b5ab36c8888
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-08-08 16:22:44 +0200

    Add Javadoc

--- core/src/main/java/io/github/almightysatan/jaskl/annotation/Entry.java
+++ core/src/main/java/io/github/almightysatan/jaskl/annotation/Entry.java
@@ -27,9 +27,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * An Annotation used to indicate that a field should be used as a config entry
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Entry {
 
+    /**
+     * The path of this entry. Uses the name of the field if empty.
+     *
+     * @return the path
+     */
     @NotNull String value() default "";
 }
