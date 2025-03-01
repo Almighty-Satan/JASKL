0aad38004b03e37d5c881e0bc570d227d918fd4b
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2025-03-01 10:16:37 +0100

    Add Javadoc

--- core/src/main/java/io/github/almightysatan/jaskl/ObjectMapper.java
+++ core/src/main/java/io/github/almightysatan/jaskl/ObjectMapper.java
@@ -26,24 +26,87 @@ import org.jetbrains.annotations.Unmodifiable;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * An {@link ObjectMapper} is used to create an object from a map of properties. This allows the user to save custom
+ * objects to a config without using annotations.
+ *
+ * @param <T> the type of the object
+ */
 public interface ObjectMapper<T> {
 
-    @NotNull T createInstance(@Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> values) throws InvalidTypeException, ValidationException;
+    /**
+     * Creates a new instance of the object from the given values.
+     *
+     * @param values A {@link Map} containing the values
+     * @return a new instance of the object
+     * @throws InvalidTypeException if a value does not match its expected {@link Type}
+     * @throws ValidationException  if a value fails validation
+     */
+    @NotNull T createInstance(@Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> values)
+            throws InvalidTypeException, ValidationException;
 
-    @Unmodifiable @NotNull Map<@NotNull String, @NotNull Object> readValues(@NotNull T instance) throws InvalidTypeException;
+    /**
+     * Creates a {@link Map} containing the values from a given instance of the object.
+     *
+     * @param instance the object
+     * @return {@link Map} containing the values
+     * @throws InvalidTypeException if a value does not match its expected {@link Type}
+     */
+    @Unmodifiable
+    @NotNull Map<@NotNull String, @NotNull Object> readValues(@NotNull T instance)
+            throws InvalidTypeException;
 
+    /**
+     * Returns the class of the object.
+     *
+     * @return the class of the object
+     */
     @NotNull Class<T> getObjectClass();
 
+    /**
+     * Returns an array containing all {@link Property Properties}.
+     *
+     * @return an array containing all {@link Property Properties}
+     */
     @NotNull Property<?> @NotNull [] getProperties();
 
+    /**
+     * Represents a property (field) of an object
+     *
+     * @param <T> the type
+     */
     interface Property<T> {
 
+        /**
+         * Returns the name of this {@link Property}.
+         *
+         * @return the name
+         */
         @NotNull String getKey();
 
+        /**
+         * Returns the {@link Type} of this {@link Property}.
+         *
+         * @return the {@link Type} of this {@link Property}
+         */
         @NotNull Type<T> getType();
 
+        /**
+         * Returns {@code true} if this {@link Property} may not always be present.
+         *
+         * @return {@code true} if this {@link Property} may not always be present
+         */
         boolean isOptional();
 
+        /**
+         * Returns a new {@link Property}.
+         *
+         * @param key      the name
+         * @param type     the {@link Type}
+         * @param optional {@code true} if this {@link Property} may not always be present
+         * @param <T>      the type
+         * @return a new {@link Property}
+         */
         static <T> Property<T> of(@NotNull String key, @NotNull Type<T> type, boolean optional) {
             Objects.requireNonNull(key);
             Objects.requireNonNull(type);
@@ -67,6 +130,14 @@ public interface ObjectMapper<T> {
             };
         }
 
+        /**
+         * Returns a new {@link Property}.
+         *
+         * @param key  the name
+         * @param type the {@link Type}
+         * @param <T>  the type
+         * @return a new {@link Property}
+         */
         static <T> Property<T> of(@NotNull String key, @NotNull Type<T> type) {
             return of(key, type, false);
         }
