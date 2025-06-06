b75b8c6cee29f911ef99f5f74c3235515cb09039
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2025-06-06 09:04:56 +0200

    Fix List and Map validator signatures

--- core/src/main/java/io/github/almightysatan/jaskl/entries/MapConfigEntry.java
+++ core/src/main/java/io/github/almightysatan/jaskl/entries/MapConfigEntry.java
@@ -46,7 +46,7 @@ public interface MapConfigEntry<K, V> extends ConfigEntry<Map<K, V>> {
      * @throws ValidationException  if the default value fails validation
      */
     @SafeVarargs
-    static <K, V> @NotNull MapConfigEntry<K, V> of(@NotNull Config config, @NotNull String path, @Nullable String description, @NotNull Map<K, V> defaultValue, @NotNull Type<K> keyType, @NotNull Type<V> valueType, @NotNull Validator<Map<? super K, ? super V>>... validators) throws InvalidTypeException, ValidationException {
+    static <K, V> @NotNull MapConfigEntry<K, V> of(@NotNull Config config, @NotNull String path, @Nullable String description, @NotNull Map<K, V> defaultValue, @NotNull Type<K> keyType, @NotNull Type<V> valueType, @NotNull Validator<? super Map<K, V>>... validators) throws InvalidTypeException, ValidationException {
         class MapConfigEntryImpl extends WritableConfigEntryImpl<Map<K, V>> implements MapConfigEntry<K, V> {
             MapConfigEntryImpl() {
                 super(Type.validated(Type.map(keyType, valueType), validators), path, description, defaultValue);
@@ -73,7 +73,7 @@ public interface MapConfigEntry<K, V> extends ConfigEntry<Map<K, V>> {
      * @throws ValidationException  if the default value fails validation
      */
     @SafeVarargs
-    static <K, V> @NotNull MapConfigEntry<K, V> of(@NotNull Config config, @NotNull String path, @NotNull Map<K, V> defaultValue, @NotNull Type<K> keyType, @NotNull Type<V> valueType, @NotNull Validator<Map<? super K, ? super V>>... validators) throws InvalidTypeException, ValidationException {
+    static <K, V> @NotNull MapConfigEntry<K, V> of(@NotNull Config config, @NotNull String path, @NotNull Map<K, V> defaultValue, @NotNull Type<K> keyType, @NotNull Type<V> valueType, @NotNull Validator<? super Map<K,V>>... validators) throws InvalidTypeException, ValidationException {
         return of(config, path, null, defaultValue, keyType, valueType, validators);
     }
 }
